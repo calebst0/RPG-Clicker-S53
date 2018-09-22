72cccf0079dae8ceac0aa007ae72c7be52db47ae
calebst <calebst@gmail.com>
2018-09-23 11:23:12 +1200

    level project
    project with level script

---
+++ Leveling System/Assets/Scripts/LevelingScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelingScript : MonoBehaviour
+{
+
+    public int currentXp;
+    public int currentLevel;
+    public int[] xpPerLevel;
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (currentXp >= xpPerLevel[currentLevel])
+        {
+            currentLevel++;
+        }
+    }
+
+    public void addExperience(int xpGained)
+    {
+        currentXp += xpGained;
+    }
+}
