b878d2b5746c7ae1a19345c470b9632e1820d5b2
recallm <42362641+recallm@users.noreply.github.com>
2018-10-21 13:46:01 +1300

    Added sprites, monsters and teleport functions to stop stuff fallig of screen

--- Assets/Scripts/HealthBarScript.cs
+++ Assets/Scripts/HealthBarScript.cs
@@ -5,28 +5,23 @@ using UnityEngine.UI;
 
 public class HealthBarScript : MonoBehaviour {
 
+    private static float DMG_APPLIER = .1f;
     public const float maxHealth = 1f;
     public float currentHealth = maxHealth;
     public Image healthbar;
+    
 
     void Start()
     {
 
         currentHealth = maxHealth;
-
-    }
-
-    void Update()
-    {
-       
-
     }
 
     public void UpdateHealth()
     {
-       currentHealth -= 0.25f; // Decrements the health bar graphic
+       currentHealth -= DMG_APPLIER; // Decrements the health bar graphic
         
-       this.healthbar.fillAmount = currentHealth;
+      this.healthbar.fillAmount = currentHealth;
     }
 
 
